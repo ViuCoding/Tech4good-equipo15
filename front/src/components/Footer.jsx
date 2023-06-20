9fabbfea57b332cd7c242bafbd94ea0595e11e00
Vincenzo Cristiano <viulenzgg@gmail.com>
2023-06-20 13:45:55 +0200

    footer

---
+++ front/src/components/Footer.jsx
@@ -0,0 +1,62 @@
+import Logo from "../assets/logo.png";
+import GitHub from "../assets/github.png";
+import Insta from "../assets/instagram.png";
+import Linkedin from "../assets/linkedin.png";
+import RSS from "../assets/rss.png";
+
+import styled from "styled-components";
+const StyledFooter = styled.footer`
+  padding: 2rem 4rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: column;
+  gap: 1rem;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
+`;
+
+const LogoStyled = styled.img`
+  width: 50px;
+`;
+
+const IconStyled = styled.img`
+  width: 30px;
+`;
+
+const IconsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`;
+
+const StyledLink = styled.a`
+  color: #1c315e;
+  font-weight: 600;
+`;
+
+export default function Footer() {
+  return (
+    <StyledFooter>
+      <div>
+        <LogoStyled src={Logo} />
+      </div>
+
+      <IconsContainer>
+        <StyledLink href='#'>Home</StyledLink>
+        <StyledLink href='#'>Graph1</StyledLink>
+        <StyledLink href='#'>Graph2</StyledLink>
+        <StyledLink href='#'>Solutions</StyledLink>
+      </IconsContainer>
+
+      <IconsContainer>
+        <IconStyled src={GitHub} />
+        <IconStyled src={Insta} />
+        <IconStyled src={Linkedin} />
+        <IconStyled src={RSS} />
+      </IconsContainer>
+    </StyledFooter>
+  );
+}
