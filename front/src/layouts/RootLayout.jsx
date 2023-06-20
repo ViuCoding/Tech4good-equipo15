6a45229e7a3069190d43c251fc13f10bfafe8b7c
Vincenzo Cristiano <94926738+ViuCoding@users.noreply.github.com>
2023-06-20 13:46:35 +0200

    footer (#1)

--- front/src/layouts/RootLayout.jsx
+++ front/src/layouts/RootLayout.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
 
 export default function RootLayout() {
   return (
@@ -7,7 +8,7 @@ export default function RootLayout() {
       <main className='content'>
         <Outlet />
       </main>
-      <footer>FOOTER</footer>
+      <Footer />
     </>
   );
 }
